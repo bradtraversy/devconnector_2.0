fa2033d2258e5c72bb719c860d1865d8c5910c3a
Brad Traversy <traversymedia@gmail.com>
2019-04-16 09:33:30 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('API Running'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
