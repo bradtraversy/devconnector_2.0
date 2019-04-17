e1968fda2b197786d785a6a4f20b906a7aef6855
Brad Traversy <traversymedia@gmail.com>
2019-04-17 16:15:24 -0400

    Delete profile & user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -145,4 +145,23 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+// @route    DELETE api/profile
+// @desc     Delete profile, user & posts
+// @access   Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // @todo - remove users posts
+
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
