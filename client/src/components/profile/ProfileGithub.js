786668459685937abf4889e3299333a3af507c4e
Diego Mais <diegomais@live.com>
2019-06-05 22:05:03 -0300

    Fix key value at repos list in ProfileGithub.js
    This update fix a warning alert: Each child in a list should have a unique "key" prop.
    Fix key of repos fetched on GitHub.

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -16,7 +16,7 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
         <Spinner />
       ) : (
         repos.map(repo => (
-          <div key={repo._id} className='repo bg-white p-1 my-1'>
+          <div key={repo.id} className='repo bg-white p-1 my-1'>
             <div>
               <h4>
                 <a
