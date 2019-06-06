29bde7e51e24a7ce6544b5316101f4d955dc350f
Diego Mais <diegomais@live.com>
2019-06-05 21:05:54 -0300

    Fix typechecking in ProfileExperience.js
    Fix typechecking in ProfileExperience component: Prop 'experience' of type 'object' supplied to 'ProfileExperience', but was declared type of 'array' by mistake.

--- client/src/components/profile/ProfileExperience.js
+++ client/src/components/profile/ProfileExperience.js
@@ -22,7 +22,7 @@ const ProfileExperience = ({
 );
 
 ProfileExperience.propTypes = {
-  experience: PropTypes.array.isRequired
+  experience: PropTypes.object.isRequired
 };
 
 export default ProfileExperience;
