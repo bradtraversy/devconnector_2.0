1bb4fc8e8fa050d98bdadc696b9764ebaf42b7d1
mahihossain <sayed.muddashir@gmail.com>
2019-10-17 10:41:35 +0600

    Added new recommended formating for hashing password in users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -12,73 +12,73 @@ const User = require('../../models/User');
 // @desc     Register user
 // @access   Public
 router.post(
-  '/',
-  [
-    check('name', 'Name is required')
-      .not()
-      .isEmpty(),
-    check('email', 'Please include a valid email').isEmail(),
-    check(
-      'password',
-      'Please enter a password with 6 or more characters'
-    ).isLength({ min: 6 })
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+	'/',
+	[
+		check('name', 'Name is required')
+			.not()
+			.isEmpty(),
+		check('email', 'Please include a valid email').isEmail(),
+		check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
 
-    const { name, email, password } = req.body;
+		const { name, email, password } = req.body;
 
-    try {
-      let user = await User.findOne({ email });
+		try {
+			let user = await User.findOne({ email });
 
-      if (user) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: 'User already exists' }] });
-      }
+			if (user) {
+				return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+			}
 
-      const avatar = gravatar.url(email, {
-        s: '200',
-        r: 'pg',
-        d: 'mm'
-      });
+			const avatar = gravatar.url(email, {
+				s: '200',
+				r: 'pg',
+				d: 'mm',
+			});
 
-      user = new User({
-        name,
-        email,
-        avatar,
-        password
-      });
+			user = new User({
+				name,
+				email,
+				avatar,
+				password,
+			});
 
-      const salt = await bcrypt.genSalt(10);
+			// Better format for doing the same thing
+			bcrypt.genSalt(saltRounds, function(err, salt) {
+				if (err) {
+					throw err;
+				} else {
+					bcrypt.hash(myPlaintextPassword, salt, function(err, hash) {
+						if (err) {
+							throw err;
+						} else {
+							user.password = hash;
+							user.save();
+						}
+					});
+				}
+			});
 
-      user.password = await bcrypt.hash(password, salt);
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
 
-      await user.save();
-
-      const payload = {
-        user: {
-          id: user.id
-        }
-      };
-
-      jwt.sign(
-        payload,
-        config.get('jwtSecret'),
-        { expiresIn: 360000 },
-        (err, token) => {
-          if (err) throw err;
-          res.json({ token });
-        }
-      );
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server error');
-    }
-  }
+			jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 360000 }, (err, token) => {
+				if (err) throw err;
+				res.json({ token });
+			});
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server error');
+		}
+	},
 );
 
 module.exports = router;
--- server.js
+++ server.js
@@ -18,12 +18,12 @@ app.use('/api/posts', require('./routes/api/posts'));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+	// Set static folder
+	app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
 }
 
 const PORT = process.env.PORT || 5000;
