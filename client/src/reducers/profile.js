95db5188f31e71524bede37b43fabefa85e176ea
Brad Traversy <traversymedia@gmail.com>
2019-11-25 09:33:55 -0500

    Fixed security issue with showing wrong users info after registering

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,57 +1,58 @@
 import {
-  GET_PROFILE,
-  PROFILE_ERROR,
-  CLEAR_PROFILE,
-  UPDATE_PROFILE,
-  GET_PROFILES,
-  GET_REPOS
+	GET_PROFILE,
+	PROFILE_ERROR,
+	CLEAR_PROFILE,
+	UPDATE_PROFILE,
+	GET_PROFILES,
+	GET_REPOS
 } from '../actions/types';
 
 const initialState = {
-  profile: null,
-  profiles: [],
-  repos: [],
-  loading: true,
-  error: {}
+	profile: null,
+	profiles: [],
+	repos: [],
+	loading: true,
+	error: {}
 };
 
 export default function(state = initialState, action) {
-  const { type, payload } = action;
+	const { type, payload } = action;
 
-  switch (type) {
-    case GET_PROFILE:
-    case UPDATE_PROFILE:
-      return {
-        ...state,
-        profile: payload,
-        loading: false
-      };
-    case GET_PROFILES:
-      return {
-        ...state,
-        profiles: payload,
-        loading: false
-      };
-    case PROFILE_ERROR:
-      return {
-        ...state,
-        error: payload,
-        loading: false
-      };
-    case CLEAR_PROFILE:
-      return {
-        ...state,
-        profile: null,
-        repos: [],
-        loading: false
-      };
-    case GET_REPOS:
-      return {
-        ...state,
-        repos: payload,
-        loading: false
-      };
-    default:
-      return state;
-  }
+	switch (type) {
+		case GET_PROFILE:
+		case UPDATE_PROFILE:
+			return {
+				...state,
+				profile: payload,
+				loading: false
+			};
+		case GET_PROFILES:
+			return {
+				...state,
+				profiles: payload,
+				loading: false
+			};
+		case PROFILE_ERROR:
+			return {
+				...state,
+				error: payload,
+				loading: false,
+				profile: null
+			};
+		case CLEAR_PROFILE:
+			return {
+				...state,
+				profile: null,
+				repos: [],
+				loading: false
+			};
+		case GET_REPOS:
+			return {
+				...state,
+				repos: payload,
+				loading: false
+			};
+		default:
+			return state;
+	}
 }
