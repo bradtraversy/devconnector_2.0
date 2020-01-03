e4fa225e5ea0168e322a07ff13852ee5fde50fcd
Lingquan Han <lingquanhan@gmail.com>
2020-01-03 10:47:21 -0800

    Update CommentForm.js
    Hey Brad, I think we should change the value of the placeholder.

--- client/src/components/post/CommentForm.js
+++ client/src/components/post/CommentForm.js
@@ -23,7 +23,7 @@ const CommentForm = ({ postId, addComment }) => {
           name='text'
           cols='30'
           rows='5'
-          placeholder='Create a post'
+          placeholder='Comment this post'
           value={text}
           onChange={e => setText(e.target.value)}
           required
