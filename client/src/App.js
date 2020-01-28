262017f7ce9bd5855223935780af74f943c58722
ArunTeltia <43555219+ArunTeltia@users.noreply.github.com>
2020-01-29 01:39:12 +0530

    Api is changed in the latest version
    Due to that there is a error showing as below 
    ./src/App.js
    Attempted import error: './components/layout/Landing' does not contain a default export (imported as 'Landing').
    This error occurred during the build time and cannot be dismissed.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing';
-import Routes from './components/routing/Routes';
+import {Navbar} from './components/layout/Navbar';
+import {Landing} from './components/layout/Landing';
+import {Routes} from './components/routing/Routes';
 
 // Redux
 import { Provider } from 'react-redux';
