8d5796e65a485f0365af86b1cd1b2cb2c277c57f
Kaan Yilmaz <dyilmaz@ogr.iu.edu.tr>
2020-01-31 10:46:05 +0300

    auth reducer set user null

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -46,7 +46,8 @@ export default function(state = initialState, action) {
         ...state,
         token: null,
         isAuthenticated: false,
-        loading: false
+        loading: false,
+        user: null
       };
     default:
       return state;
