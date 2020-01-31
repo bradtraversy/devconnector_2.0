7f52979b20e3962b6260b6b7c95a54ff168d7f3e
Will Adams <bushblade@gmail.com>
2020-01-31 19:12:18 +0000

    removed unused cases and added error on default

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -7,17 +7,17 @@ import {
   LOGIN_FAIL,
   LOGOUT,
   ACCOUNT_DELETED
-} from '../actions/types';
+} from '../actions/types'
 
 const initialState = {
   token: localStorage.getItem('token'),
   isAuthenticated: null,
   loading: true,
   user: null
-};
+}
 
 export default function(state = initialState, action) {
-  const { type, payload } = action;
+  const { type, payload } = action
 
   switch (type) {
     case USER_LOADED:
@@ -26,29 +26,25 @@ export default function(state = initialState, action) {
         isAuthenticated: true,
         loading: false,
         user: payload
-      };
+      }
     case REGISTER_SUCCESS:
     case LOGIN_SUCCESS:
-      localStorage.setItem('token', payload.token);
+      localStorage.setItem('token', payload.token)
       return {
         ...state,
         ...payload,
         isAuthenticated: true,
         loading: false
-      };
-    case REGISTER_FAIL:
-    case AUTH_ERROR:
-    case LOGIN_FAIL:
-    case LOGOUT:
+      }
     case ACCOUNT_DELETED:
-      localStorage.removeItem('token');
+      localStorage.removeItem('token')
       return {
         ...state,
         token: null,
         isAuthenticated: false,
         loading: false
-      };
+      }
     default:
-      return state;
+      throw Error(`Unsupported type: ${action.type}`)
   }
 }
