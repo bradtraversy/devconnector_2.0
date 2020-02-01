5111f3d4930307d22f541302c1156476f4f5ca0f
Will Adams <bushblade@gmail.com>
2020-02-01 15:07:45 +0000

    useEffect missing dependency

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -11,18 +11,18 @@ import { getPost } from '../../actions/post';
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
     getPost(match.params.id);
-  }, [getPost]);
+  }, [getPost, match.params.id]);
 
   return loading || post === null ? (
     <Spinner />
   ) : (
     <Fragment>
-      <Link to='/posts' className='btn'>
+      <Link to="/posts" className="btn">
         Back To Posts
       </Link>
       <PostItem post={post} showActions={false} />
       <CommentForm postId={post._id} />
-      <div className='comments'>
+      <div className="comments">
         {post.comments.map(comment => (
           <CommentItem key={comment._id} comment={comment} postId={post._id} />
         ))}
@@ -40,7 +40,4 @@ const mapStateToProps = state => ({
   post: state.post
 });
 
-export default connect(
-  mapStateToProps,
-  { getPost }
-)(Post);
+export default connect(mapStateToProps, { getPost })(Post);
