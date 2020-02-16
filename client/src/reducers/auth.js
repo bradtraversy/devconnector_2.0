57531c10be9daf75a05d5aaf2d9717a482ff35ed
Will Adams <bushblade@gmail.com>
2020-02-16 18:56:51 +0000

    remove user on logout

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -51,7 +51,8 @@ export default function(state = initialState, action) {
         ...state,
         token: null,
         isAuthenticated: false,
-        loading: false
+        loading: false,
+        user: null
       };
     default:
       return state;
