34637288afa35df06fb374ba3c813bb53d03b766
Will Adams <bushblade@gmail.com>
2020-02-20 11:41:49 +0000

    basics of subscription

--- client/src/store.js
+++ client/src/store.js
@@ -13,4 +13,18 @@ const store = createStore(
   composeWithDevTools(applyMiddleware(...middleware))
 );
 
+// set up a store subscription listener
+
+let currentState;
+
+store.subscribe(() => {
+  let previousState = currentState;
+  currentState = store.getState();
+  console.log('previous state', previousState);
+  console.log('current state', currentState);
+  if (previousState.auth.token !== currentState.auth.token) {
+    localStorage.setItem('token', currentState.auth.token);
+  }
+});
+
 export default store;
