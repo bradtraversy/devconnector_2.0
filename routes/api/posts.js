2023164bbf333588c0b86c721bd2c552f46d3359
Will Adams <bushblade@gmail.com>
2020-02-24 09:07:51 +0000

    cleaned up delete comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -210,6 +210,15 @@ router.post(
 router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
+
+    // Pull out comment
+    const comment = post.comments.find(
+      comment => comment.id === req.params.comment_id
+    );
+    // Make sure comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment does not exist' });
+    }
     // Check user
     if (comment.user.toString() !== req.user.id) {
       return res.status(401).json({ msg: 'User not authorized' });
