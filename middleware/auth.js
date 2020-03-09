688c393b2710c131dd36fe779cad9af57458b1d3
Will Adams <bushblade@gmail.com>
2020-03-09 16:14:49 +0000

    remove unused async await from commit 734d4cdf417e

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
 
-module.exports = async function(req, res, next) {
+module.exports = function(req, res, next) {
   // Get token from header
   const token = req.header('x-auth-token');
 
@@ -12,17 +12,16 @@ module.exports = async function(req, res, next) {
 
   // Verify token
   try {
-    await jwt.verify(token, config.get('jwtSecret'), (error, decoded)=>{
-      if(error){
+    jwt.verify(token, config.get('jwtSecret'), (error, decoded) => {
+      if (error) {
         res.status(401).json({ msg: 'Token is not valid' });
-      }
-      else{
+      } else {
         req.user = decoded.user;
         next();
       }
     });
   } catch (err) {
-    console.error('something wrong with auth middleware')
+    console.error('something wrong with auth middleware');
     res.status(500).json({ msg: 'Server Error' });
   }
 };
