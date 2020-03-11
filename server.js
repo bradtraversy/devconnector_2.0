7eec6a132bff2945c3557f58b27ba593463a02f7
David Jacoby <jacoby934@gmail.com>
2020-03-11 14:12:21 -0400

    Removed "extended: false" option
    Removed "extended: false" option from the express.json() call. This is an invalid option for expression.json() per Express.js documentation.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 connectDB();
 
 // Init Middleware
-app.use(express.json({ extended: false }));
+app.use(express.json());
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
