0b1228a8c098550a92e64dba0701f5a73533437f
Will Adams <bushblade@gmail.com>
2020-03-12 09:15:23 +0000

    remove unused var/Profile

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,7 +4,6 @@ const { check, validationResult } = require('express-validator');
 const auth = require('../../middleware/auth');
 
 const Post = require('../../models/Post');
-const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
 // @route    POST api/posts
