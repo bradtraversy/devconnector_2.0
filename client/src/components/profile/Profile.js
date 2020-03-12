22b852be7ce1eb1fb22c89f883709c16a7c70bfb
Will Adams <bushblade@gmail.com>
2020-03-12 14:49:29 +0000

    fix for #89

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -16,9 +16,10 @@ const Profile = ({
   auth,
   match
 }) => {
+  const nullProfile = !profile;
   useEffect(() => {
     getProfileById(match.params.id);
-  }, [getProfileById, match.params.id]);
+  }, [getProfileById, match.params.id, nullProfile]);
 
   return (
     <Fragment>
@@ -26,21 +27,21 @@ const Profile = ({
         <Spinner />
       ) : (
         <Fragment>
-          <Link to='/profiles' className='btn btn-light'>
+          <Link to="/profiles" className="btn btn-light">
             Back To Profiles
           </Link>
           {auth.isAuthenticated &&
             auth.loading === false &&
             auth.user._id === profile.user._id && (
-              <Link to='/edit-profile' className='btn btn-dark'>
+              <Link to="/edit-profile" className="btn btn-dark">
                 Edit Profile
               </Link>
             )}
-          <div className='profile-grid my-1'>
+          <div className="profile-grid my-1">
             <ProfileTop profile={profile} />
             <ProfileAbout profile={profile} />
-            <div className='profile-exp bg-white p-2'>
-              <h2 className='text-primary'>Experience</h2>
+            <div className="profile-exp bg-white p-2">
+              <h2 className="text-primary">Experience</h2>
               {profile.experience.length > 0 ? (
                 <Fragment>
                   {profile.experience.map(experience => (
@@ -55,8 +56,8 @@ const Profile = ({
               )}
             </div>
 
-            <div className='profile-edu bg-white p-2'>
-              <h2 className='text-primary'>Education</h2>
+            <div className="profile-edu bg-white p-2">
+              <h2 className="text-primary">Education</h2>
               {profile.education.length > 0 ? (
                 <Fragment>
                   {profile.education.map(education => (
@@ -92,7 +93,4 @@ const mapStateToProps = state => ({
   auth: state.auth
 });
 
-export default connect(
-  mapStateToProps,
-  { getProfileById }
-)(Profile);
+export default connect(mapStateToProps, { getProfileById })(Profile);
