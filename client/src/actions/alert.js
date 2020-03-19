bebf49d5c560693ef1edce5039707d7fdd447b1c
Will Adams <bushblade@gmail.com>
2020-03-19 11:26:52 +0000

    fix uuid import

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,8 +1,8 @@
-import uuid from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
 export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
-  const id = uuid.v4();
+  const id = uuidv4();
   dispatch({
     type: SET_ALERT,
     payload: { msg, alertType, id }
