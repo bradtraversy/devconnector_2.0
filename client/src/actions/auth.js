f6f045d965df072abc0523c14f3847f6d96d06db
Will Adams <bushblade@gmail.com>
2020-04-15 13:40:02 +0100

    load user after register

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -44,7 +44,7 @@ export const register = ({ name, email, password }) => async (dispatch) => {
       type: REGISTER_SUCCESS,
       payload: res.data
     });
-    dispatch(loadUser);
+    dispatch(loadUser());
   } catch (err) {
     const errors = err.response.data.errors;
 
