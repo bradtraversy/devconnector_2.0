cae6a86a3ab9fb7d33ca96155438ebec654c0a7f
Will Adams <bushblade@gmail.com>
2020-04-15 08:50:16 +0100

    no need to check loading now handled in PrivateRoute

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,27 +1,24 @@
 import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import Spinner from '../layout/Spinner';
 import PostItem from './PostItem';
 import PostForm from './PostForm';
 import { getPosts } from '../../actions/post';
 
-const Posts = ({ getPosts, post: { posts, loading } }) => {
+const Posts = ({ getPosts, post: { posts } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
 
-  return loading ? (
-    <Spinner />
-  ) : (
+  return (
     <Fragment>
-      <h1 className='large text-primary'>Posts</h1>
-      <p className='lead'>
-        <i className='fas fa-user' /> Welcome to the community
+      <h1 className="large text-primary">Posts</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Welcome to the community
       </p>
       <PostForm />
-      <div className='posts'>
-        {posts.map(post => (
+      <div className="posts">
+        {posts.map((post) => (
           <PostItem key={post._id} post={post} />
         ))}
       </div>
@@ -34,11 +31,8 @@ Posts.propTypes = {
   post: PropTypes.object.isRequired
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   post: state.post
 });
 
-export default connect(
-  mapStateToProps,
-  { getPosts }
-)(Posts);
+export default connect(mapStateToProps, { getPosts })(Posts);
