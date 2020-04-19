22977d7802401544ea309ea3b295dd322129eaa0
Will Adams <bushblade@gmail.com>
2020-04-19 11:39:33 +0100

    remove unused ref

--- models/Post.js
+++ models/Post.js
@@ -3,8 +3,7 @@ const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
   user: {
-    type: Schema.Types.ObjectId,
-    ref: 'users'
+    type: Schema.Types.ObjectId
   },
   text: {
     type: String,
@@ -19,16 +18,14 @@ const PostSchema = new Schema({
   likes: [
     {
       user: {
-        type: Schema.Types.ObjectId,
-        ref: 'users'
+        type: Schema.Types.ObjectId
       }
     }
   ],
   comments: [
     {
       user: {
-        type: Schema.Types.ObjectId,
-        ref: 'users'
+        type: Schema.Types.ObjectId
       },
       text: {
         type: String,
@@ -52,4 +49,4 @@ const PostSchema = new Schema({
   }
 });
 
-module.exports = Post = mongoose.model('post', PostSchema);
+module.exports = mongoose.model('post', PostSchema);
