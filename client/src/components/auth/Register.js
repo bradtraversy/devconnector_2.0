ea645a5b4743bdc52a3dab435de8bec035d65401
Will Adams <bushblade@gmail.com>
2020-05-05 15:43:19 +0100

    remove anonymous function

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -15,10 +15,10 @@ const Register = ({ setAlert, register, isAuthenticated }) => {
 
   const { name, email, password, password2 } = formData;
 
-  const onChange = e =>
+  const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const onSubmit = async e => {
+  const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
       setAlert('Passwords do not match', 'danger');
@@ -28,60 +28,60 @@ const Register = ({ setAlert, register, isAuthenticated }) => {
   };
 
   if (isAuthenticated) {
-    return <Redirect to='/dashboard' />;
+    return <Redirect to="/dashboard" />;
   }
 
   return (
     <Fragment>
-      <h1 className='large text-primary'>Sign Up</h1>
-      <p className='lead'>
-        <i className='fas fa-user' /> Create Your Account
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Create Your Account
       </p>
-      <form className='form' onSubmit={e => onSubmit(e)}>
-        <div className='form-group'>
+      <form className="form" onSubmit={onSubmit}>
+        <div className="form-group">
           <input
-            type='text'
-            placeholder='Name'
-            name='name'
+            type="text"
+            placeholder="Name"
+            name="name"
             value={name}
-            onChange={e => onChange(e)}
+            onChange={onChange}
           />
         </div>
-        <div className='form-group'>
+        <div className="form-group">
           <input
-            type='email'
-            placeholder='Email Address'
-            name='email'
+            type="email"
+            placeholder="Email Address"
+            name="email"
             value={email}
-            onChange={e => onChange(e)}
+            onChange={onChange}
           />
-          <small className='form-text'>
+          <small className="form-text">
             This site uses Gravatar so if you want a profile image, use a
             Gravatar email
           </small>
         </div>
-        <div className='form-group'>
+        <div className="form-group">
           <input
-            type='password'
-            placeholder='Password'
-            name='password'
+            type="password"
+            placeholder="Password"
+            name="password"
             value={password}
-            onChange={e => onChange(e)}
+            onChange={onChange}
           />
         </div>
-        <div className='form-group'>
+        <div className="form-group">
           <input
-            type='password'
-            placeholder='Confirm Password'
-            name='password2'
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
             value={password2}
-            onChange={e => onChange(e)}
+            onChange={onChange}
           />
         </div>
-        <input type='submit' className='btn btn-primary' value='Register' />
+        <input type="submit" className="btn btn-primary" value="Register" />
       </form>
-      <p className='my-1'>
-        Already have an account? <Link to='/login'>Sign In</Link>
+      <p className="my-1">
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </Fragment>
   );
@@ -93,11 +93,8 @@ Register.propTypes = {
   isAuthenticated: PropTypes.bool
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   isAuthenticated: state.auth.isAuthenticated
 });
 
-export default connect(
-  mapStateToProps,
-  { setAlert, register }
-)(Register);
+export default connect(mapStateToProps, { setAlert, register })(Register);
