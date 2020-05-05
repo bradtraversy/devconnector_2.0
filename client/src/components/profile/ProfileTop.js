dde475a18d5f0ec439afafd67cf83d2adddd3df7
uowyuchen <uowyuchen@gmail.com>
2020-05-04 23:29:20 -0400

    Update client's social links
    update from <a href={website}></a> to <a href={`//${website}`}></a>
    because the previous code caused the error when you clicked on it. 
    for example, your website is "www.youtube.com", when you clicked it, it showed "localhost:3000/www.youtube.com"

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -21,32 +21,32 @@ const ProfileTop = ({
       <p>{location && <span>{location}</span>}</p>
       <div className='icons my-1'>
         {website && (
-          <a href={website} target='_blank' rel='noopener noreferrer'>
+          <a href={`//${website}`} target='_blank' rel='noopener noreferrer'>
             <i className='fas fa-globe fa-2x' />
           </a>
         )}
         {social && social.twitter && (
-          <a href={social.twitter} target='_blank' rel='noopener noreferrer'>
+          <a href={`//${social.twitter}`} target='_blank' rel='noopener noreferrer'>
             <i className='fab fa-twitter fa-2x' />
           </a>
         )}
         {social && social.facebook && (
-          <a href={social.facebook} target='_blank' rel='noopener noreferrer'>
+          <a href={//${social.facebook}} target='_blank' rel='noopener noreferrer'>
             <i className='fab fa-facebook fa-2x' />
           </a>
         )}
         {social && social.linkedin && (
-          <a href={social.linkedin} target='_blank' rel='noopener noreferrer'>
+          <a href={`//${social.linkedin}`} target='_blank' rel='noopener noreferrer'>
             <i className='fab fa-linkedin fa-2x' />
           </a>
         )}
         {social && social.youtube && (
-          <a href={social.youtube} target='_blank' rel='noopener noreferrer'>
+          <a href={`//${social.youtube}`} target='_blank' rel='noopener noreferrer'>
             <i className='fab fa-youtube fa-2x' />
           </a>
         )}
         {social && social.instagram && (
-          <a href={social.instagram} target='_blank' rel='noopener noreferrer'>
+          <a href={`//${social.instagram}`} target='_blank' rel='noopener noreferrer'>
             <i className='fab fa-instagram fa-2x' />
           </a>
         )}
