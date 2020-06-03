3917ae6ad0df71c38a19e7786425ffe4977e5e97
Will Adams <bushblade@gmail.com>
2020-06-03 08:26:08 +0100

    remove CLEAR_PROFILE from logout action

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -80,6 +80,5 @@ export const login = (email, password) => async dispatch => {
 
 // Logout / Clear Profile
 export const logout = () => dispatch => {
-  dispatch({ type: CLEAR_PROFILE });
   dispatch({ type: LOGOUT });
 };
