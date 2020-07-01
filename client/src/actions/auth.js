601df2294e1ff849e43d8c6736a57aa7044274de
mohammad saad <mostafasaad55@gmail.com>
2020-07-01 20:52:26 +0200

    fix post actions

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,16 +2,16 @@ import api from '../utils/api';
 import { setAlert } from './alert';
 import {
   REGISTER_SUCCESS,
-  REGISTER_FAIL,
+  // REGISTER_FAIL,
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL,
+  // LOGIN_FAIL,
   LOGOUT
 } from './types';
 
 // Load User
-export const loadUser = () => async dispatch => {
+export const loadUser = () => async (dispatch) => {
   try {
     const res = await api.get('/auth');
 
@@ -27,7 +27,7 @@ export const loadUser = () => async dispatch => {
 };
 
 // Register User
-export const register = formData => async dispatch => {
+export const register = (formData) => async (dispatch) => {
   try {
     const res = await api.post('/users', formData);
 
@@ -40,17 +40,17 @@ export const register = formData => async dispatch => {
     const errors = err.response.data.errors;
 
     if (errors) {
-      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
     }
 
     dispatch({
-      type: REGISTER_FAIL
+      type: AUTH_ERROR
     });
   }
 };
 
 // Login User
-export const login = (email, password) => async dispatch => {
+export const login = (email, password) => async (dispatch) => {
   const body = { email, password };
 
   try {
@@ -66,11 +66,11 @@ export const login = (email, password) => async dispatch => {
     const errors = err.response.data.errors;
 
     if (errors) {
-      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
     }
 
     dispatch({
-      type: LOGIN_FAIL
+      type: AUTH_ERROR
     });
   }
 };
