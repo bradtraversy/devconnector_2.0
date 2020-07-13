912eacf49879eca06ec21770a7ac01d894ba4a97
Behnaz Pourafshari <behnaz.po@gmail.com>
2020-07-13 15:45:14 +0100

    no need to check loading now handled in PrivateRoute

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -1,37 +1,30 @@
 import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import Spinner from '../layout/Spinner';
 import ProfileItem from './ProfileItem';
 import { getProfiles } from '../../actions/profile';
 
-const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
+const Profiles = ({ getProfiles, profile: { profiles} }) => {
   useEffect(() => {
     getProfiles();
   }, [getProfiles]);
 
   return (
     <Fragment>
-      {loading ? (
-        <Spinner />
-      ) : (
-        <Fragment>
-          <h1 className='large text-primary'>Developers</h1>
-          <p className='lead'>
-            <i className='fab fa-connectdevelop' /> Browse and connect with
-            developers
-          </p>
-          <div className='profiles'>
-            {profiles.length > 0 ? (
-              profiles.map(profile => (
-                <ProfileItem key={profile._id} profile={profile} />
-              ))
-            ) : (
-              <h4>No profiles found...</h4>
-            )}
-          </div>
-        </Fragment>
-      )}
+      <h1 className="large text-primary">Developers</h1>
+      <p className="lead">
+        <i className="fab fa-connectdevelop" /> Browse and connect with
+        developers
+      </p>
+      <div className="profiles">
+        {profiles.length > 0 ? (
+          profiles.map((profile) => (
+            <ProfileItem key={profile._id} profile={profile} />
+          ))
+        ) : (
+          <h4>No profiles found...</h4>
+        )}
+      </div>
     </Fragment>
   );
 };
