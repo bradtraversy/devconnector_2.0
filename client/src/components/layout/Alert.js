b5356dbdac5ee0f83d5559d32a18c2b19366840d
Will Adams <bushblade@gmail.com>
2020-10-14 18:28:17 +0100

    remove redundant checks

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -3,9 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 const Alert = ({ alerts }) =>
-  alerts !== null &&
-  alerts.length > 0 &&
-  alerts.map(alert => (
+  alerts.map((alert) => (
     <div key={alert.id} className={`alert alert-${alert.alertType}`}>
       {alert.msg}
     </div>
@@ -15,7 +13,7 @@ Alert.propTypes = {
   alerts: PropTypes.array.isRequired
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   alerts: state.alert
 });
 
