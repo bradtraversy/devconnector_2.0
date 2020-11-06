d9726c27a6d51df3d23b75958ef96d2810de8e1b
Will Adams <bushblade@gmail.com>
2020-11-06 16:53:16 +0000

    clean up and better commenting

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -37,54 +37,51 @@ router.get('/me', auth, async (req, res) => {
 // @access   Private
 router.post(
   '/',
-  [
-    auth,
-    [
-      check('status', 'Status is required').not().isEmpty(),
-      check('skills', 'Skills is required').not().isEmpty()
-    ]
-  ],
+  auth,
+  check('status', 'Status is required').not().isEmpty(),
+  check('skills', 'Skills is required').not().isEmpty(),
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+
+    // destructure the request
     const {
-      company,
-      location,
       website,
-      bio,
       skills,
-      status,
-      githubusername,
       youtube,
       twitter,
       instagram,
       linkedin,
-      facebook
+      facebook,
+      // spread the rest of the fields we don't need to check
+      ...rest
     } = req.body;
 
+    // build a profile
     const profileFields = {
       user: req.user.id,
-      company,
-      location,
-      website: website && website !== '' ? normalize(website, { forceHttps: true }) : '',
-      bio,
+      website:
+        website && website !== ''
+          ? normalize(website, { forceHttps: true })
+          : '',
       skills: Array.isArray(skills)
         ? skills
         : skills.split(',').map((skill) => ' ' + skill.trim()),
-      status,
-      githubusername
+      ...rest
     };
 
-    // Build social object and add to profileFields
-    const socialfields = { youtube, twitter, instagram, linkedin, facebook };
+    // Build socialFields object
+    const socialFields = { youtube, twitter, instagram, linkedin, facebook };
 
-    for (const [key, value] of Object.entries(socialfields)) {
+    // normalize social fields to ensure valid url
+    for (const [key, value] of Object.entries(socialFields)) {
       if (value && value.length > 0)
-        socialfields[key] = normalize(value, { forceHttps: true });
+        socialFields[key] = normalize(value, { forceHttps: true });
     }
-    profileFields.social = socialfields;
+    // add to profileFields
+    profileFields.social = socialFields;
 
     try {
       // Using upsert option (creates new doc if no match is found):
@@ -93,10 +90,10 @@ router.post(
         { $set: profileFields },
         { new: true, upsert: true, setDefaultsOnInsert: true }
       );
-      res.json(profile);
+      return res.json(profile);
     } catch (err) {
       console.error(err.message);
-      res.status(500).send('Server Error');
+      return res.status(500).send('Server Error');
     }
   }
 );
