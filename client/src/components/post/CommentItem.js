02de6aad0665cdcb1fd5555c1290bcc572b87f69
Will Adams <bushblade@gmail.com>
2020-11-09 21:27:03 +0000

    remove Moment from CommentItem

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import Moment from 'react-moment';
+import formatDate from '../../utils/formatDate';
 import { deleteComment } from '../../actions/post';
 
 const CommentItem = ({
@@ -11,25 +11,23 @@ const CommentItem = ({
   auth,
   deleteComment
 }) => (
-  <div className='post bg-white p-1 my-1'>
+  <div className="post bg-white p-1 my-1">
     <div>
       <Link to={`/profile/${user}`}>
-        <img className='round-img' src={avatar} alt='' />
+        <img className="round-img" src={avatar} alt="" />
         <h4>{name}</h4>
       </Link>
     </div>
     <div>
-      <p className='my-1'>{text}</p>
-      <p className='post-date'>
-        Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
-      </p>
+      <p className="my-1">{text}</p>
+      <p className="post-date">Posted on {formatDate(date)}</p>
       {!auth.loading && user === auth.user._id && (
         <button
           onClick={() => deleteComment(postId, _id)}
-          type='button'
-          className='btn btn-danger'
+          type="button"
+          className="btn btn-danger"
         >
-          <i className='fas fa-times' />
+          <i className="fas fa-times" />
         </button>
       )}
     </div>
@@ -43,11 +41,8 @@ CommentItem.propTypes = {
   deleteComment: PropTypes.func.isRequired
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   auth: state.auth
 });
 
-export default connect(
-  mapStateToProps,
-  { deleteComment }
-)(CommentItem);
+export default connect(mapStateToProps, { deleteComment })(CommentItem);
