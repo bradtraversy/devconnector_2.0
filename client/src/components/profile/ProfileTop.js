77c2b555c48ed993037787efee411efd1b9dc091
Kevin Uriel Fonseca <kebin1421@hotmail.com>
2020-12-21 01:40:22 -0600

    Revert "Update ProfileTop.js have a cleaner JS code."

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -25,12 +25,31 @@ const ProfileTop = ({
             <i className='fas fa-globe fa-2x' />
           </a>
         )}
-        {social &&
-          Object.entries(social).map(([key, value]) => (
-            <a key={key} href={value} target='_blank' rel='noopener noreferrer'>
-              <i className={`fab fa-${key} fa-2x`}></i>
-            </a>
-          ))}
+        {social && social.twitter && (
+          <a href={social.twitter} target='_blank' rel='noopener noreferrer'>
+            <i className='fab fa-twitter fa-2x' />
+          </a>
+        )}
+        {social && social.facebook && (
+          <a href={social.facebook} target='_blank' rel='noopener noreferrer'>
+            <i className='fab fa-facebook fa-2x' />
+          </a>
+        )}
+        {social && social.linkedin && (
+          <a href={social.linkedin} target='_blank' rel='noopener noreferrer'>
+            <i className='fab fa-linkedin fa-2x' />
+          </a>
+        )}
+        {social && social.youtube && (
+          <a href={social.youtube} target='_blank' rel='noopener noreferrer'>
+            <i className='fab fa-youtube fa-2x' />
+          </a>
+        )}
+        {social && social.instagram && (
+          <a href={social.instagram} target='_blank' rel='noopener noreferrer'>
+            <i className='fab fa-instagram fa-2x' />
+          </a>
+        )}
       </div>
     </div>
   );
