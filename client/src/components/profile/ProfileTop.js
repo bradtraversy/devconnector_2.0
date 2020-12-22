b9b78fe7f565afbc0c3eb6c48a529e52abdbbaa1
Will Adams <bushblade@gmail.com>
2020-12-22 11:12:46 +0000

    missed at last PR correction

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -12,44 +12,32 @@ const ProfileTop = ({
   }
 }) => {
   return (
-    <div className='profile-top bg-primary p-2'>
-      <img className='round-img my-1' src={avatar} alt='' />
-      <h1 className='large'>{name}</h1>
-      <p className='lead'>
+    <div className="profile-top bg-primary p-2">
+      <img className="round-img my-1" src={avatar} alt="" />
+      <h1 className="large">{name}</h1>
+      <p className="lead">
         {status} {company && <span> at {company}</span>}
       </p>
       <p>{location && <span>{location}</span>}</p>
-      <div className='icons my-1'>
+      <div className="icons my-1">
         {website && (
-          <a href={website} target='_blank' rel='noopener noreferrer'>
-            <i className='fas fa-globe fa-2x' />
+          <a href={website} target="_blank" rel="noopener noreferrer">
+            <i className="fas fa-globe fa-2x" />
           </a>
         )}
-        {social && social.twitter && (
-          <a href={social.twitter} target='_blank' rel='noopener noreferrer'>
-            <i className='fab fa-twitter fa-2x' />
-          </a>
-        )}
-        {social && social.facebook && (
-          <a href={social.facebook} target='_blank' rel='noopener noreferrer'>
-            <i className='fab fa-facebook fa-2x' />
-          </a>
-        )}
-        {social && social.linkedin && (
-          <a href={social.linkedin} target='_blank' rel='noopener noreferrer'>
-            <i className='fab fa-linkedin fa-2x' />
-          </a>
-        )}
-        {social && social.youtube && (
-          <a href={social.youtube} target='_blank' rel='noopener noreferrer'>
-            <i className='fab fa-youtube fa-2x' />
-          </a>
-        )}
-        {social && social.instagram && (
-          <a href={social.instagram} target='_blank' rel='noopener noreferrer'>
-            <i className='fab fa-instagram fa-2x' />
-          </a>
-        )}
+        {social &&
+          Object.entries(social)
+            .filter(([key, value]) => value)
+            .map(([key, value]) => (
+              <a
+                key={key}
+                href={value}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className={`fab fa-${key} fa-2x`}></i>
+              </a>
+            ))}
       </div>
     </div>
   );
