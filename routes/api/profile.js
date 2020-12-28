227c9d7f28eb10cf1c2226cf2c36a8173b0794f2
Will Adams <bushblade@gmail.com>
2020-12-28 12:41:08 +0000

    remove uneccessary nested arrays

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -157,17 +157,13 @@ router.delete('/', auth, async (req, res) => {
 // @access   Private
 router.put(
   '/experience',
-  [
-    auth,
-    [
-      check('title', 'Title is required').not().isEmpty(),
-      check('company', 'Company is required').not().isEmpty(),
-      check('from', 'From date is required and needs to be from the past')
-        .not()
-        .isEmpty()
-        .custom((value, { req }) => (req.body.to ? value < req.body.to : true))
-    ]
-  ],
+  auth,
+  check('title', 'Title is required').not().isEmpty(),
+  check('company', 'Company is required').not().isEmpty(),
+  check('from', 'From date is required and needs to be from the past')
+    .not()
+    .isEmpty()
+    .custom((value, { req }) => (req.body.to ? value < req.body.to : true)),
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -234,18 +230,14 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
 // @access   Private
 router.put(
   '/education',
-  [
-    auth,
-    [
-      check('school', 'School is required').not().isEmpty(),
-      check('degree', 'Degree is required').not().isEmpty(),
-      check('fieldofstudy', 'Field of study is required').not().isEmpty(),
-      check('from', 'From date is required and needs to be from the past')
-        .not()
-        .isEmpty()
-        .custom((value, { req }) => (req.body.to ? value < req.body.to : true))
-    ]
-  ],
+  auth,
+  check('school', 'School is required').not().isEmpty(),
+  check('degree', 'Degree is required').not().isEmpty(),
+  check('fieldofstudy', 'Field of study is required').not().isEmpty(),
+  check('from', 'From date is required and needs to be from the past')
+    .not()
+    .isEmpty()
+    .custom((value, { req }) => (req.body.to ? value < req.body.to : true)),
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
