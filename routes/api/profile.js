9cecd65e270813869d540d2ef856976f7a9bbc1b
Will Adams <bushblade@gmail.com>
2020-12-28 12:54:54 +0000

    async deletion

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -139,11 +139,13 @@ router.get(
 router.delete('/', auth, async (req, res) => {
   try {
     // Remove user posts
-    await Post.deleteMany({ user: req.user.id });
     // Remove profile
-    await Profile.findOneAndRemove({ user: req.user.id });
     // Remove user
-    await User.findOneAndRemove({ _id: req.user.id });
+    await Promise.all([
+      Post.deleteMany({ user: req.user.id }),
+      Profile.findOneAndRemove({ user: req.user.id }),
+      User.findOneAndRemove({ _id: req.user.id })
+    ]);
 
     res.json({ msg: 'User deleted' });
   } catch (err) {
