b14df524277c68d4228cb969a2aebdde0e398eed
GhassanElnemr <79877777+GhassanElnemr@users.noreply.github.com>
2021-03-11 14:34:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
-
 const app = express();
-
 // Connect Database
 connectDB();
-
 // Init Middleware
 app.use(express.json());
-
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
