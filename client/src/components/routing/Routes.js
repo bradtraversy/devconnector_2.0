2d9a0cf1286e304ee6e609a657f7a992a25b2d6b
Will Adams <bushblade@gmail.com>
2021-08-12 08:05:30 +0100

    remove unused declaration

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -14,7 +14,7 @@ import Post from '../post/Post';
 import NotFound from '../layout/NotFound';
 import PrivateRoute from './PrivateRoute';
 
-const Routes = props => {
+const Routes = () => {
   return (
     <section className="container">
       <Alert />
