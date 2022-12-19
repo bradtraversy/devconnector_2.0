a3ad2b2adbfcc0935fc3e226b642d087658a488a
Will Adams <bushblade@gmail.com>
2022-12-19 11:14:33 +0000

    set up RTK

--- client/src/store.js
+++ client/src/store.js
@@ -1,18 +1,18 @@
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './reducers/auth';
+import alertReducer from './reducers/alert';
+import profileReducer from './reducers/profile';
+import postReducer from './reducers/post';
 import setAuthToken from './utils/setAuthToken';
 
-const initialState = {};
-
-const middleware = [thunk];
-
-const store = createStore(
-  rootReducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
+const store = configureStore({
+  reducer: {
+    alert: alertReducer,
+    auth: authReducer,
+    profile: profileReducer,
+    post: postReducer
+  }
+});
 
 /*
   NOTE: set up a store subscription listener
